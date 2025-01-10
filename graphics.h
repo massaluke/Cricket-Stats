453ed1f113b5aba4e94fd68a33c3b61d1721ee89
massaluke <125191181+massaluke@users.noreply.github.com>
2025-01-10 16:15:03 +1100

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,20 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <Arduino.h>
+
+// Bitmap definitions
+extern const uint8_t ADS[];
+extern const uint8_t BRH[];
+extern const uint8_t HBH[];
+extern const uint8_t MLR[];
+extern const uint8_t MLS[];
+extern const uint8_t PRS[];
+extern const uint8_t SYS[];
+extern const uint8_t SYT[];
+extern const unsigned char *wifiGIF[7];
+extern const unsigned char *ballGIF[7];
+extern const unsigned char dataLoad[];
+extern const unsigned char noSpace[];
+
+#endif // GRAPHICS_H
